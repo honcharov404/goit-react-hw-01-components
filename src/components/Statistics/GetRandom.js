0b8de7f6393025baab2343975ab4792c2ea27986
Oleksandr Honcharov <o.v.goncharov90@gmail.com>
2022-11-18 16:45:28 +0200

    add random bgc task 2

--- src/components/Statistics/GetRandom.js
+++ src/components/Statistics/GetRandom.js
@@ -1,3 +1,10 @@
-export function getRandom(min, max) {
+function getRandom(min, max) {
   return Math.ceil(Math.random() * (max - min) + min);
+}
+
+export function getRandomHexColor() {
+  return `rgb(${getRandom(25, 255)}, ${getRandom(25, 255)}, ${getRandom(
+    25,
+    255
+  )})`;
 }
--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -2,23 +2,28 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 import s from './Statistics.module.css';
+import { getRandomHexColor } from './GetRandom';
 
 export const Statistics = ({ title, stats }) => {
-  return (    
-      <section className={s.statistics}>
-        {title && <h2 className={s.title}>{title}</h2>}
+  return (
+    <section className={s.statistics}>
+      {title && <h2 className={s.title}>{title}</h2>}
 
-        <ul className={s.statList}>
-          {stats.map(({ id, label, percentage }) => {
-            return (
-              <li className={s.item} key={id}>
-                <span className={s.label}>{label}</span>
-                <span className={s.percentage}>{percentage}</span>
-              </li>
-            );
-          })}
-        </ul>
-      </section>   
+      <ul className={s.statList}>
+        {stats.map(({ id, label, percentage }) => {
+          return (
+            <li
+              className={s.item}
+              key={id}
+              style={{ backgroundColor: getRandomHexColor() }}
+            >
+              <span className={s.label}>{label}</span>
+              <span className={s.percentage}>{percentage}</span>
+            </li>
+          );
+        })}
+      </ul>
+    </section>
   );
 };
 
